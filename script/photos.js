23e4e55b3a5fff3f315850cec49c27341da64157
Jessica Montserrat Morales Enrique <jessica.morales5556@alumnos.udg.mx>
2025-04-13 03:26:06 -0600

    Add modifications in carrousel

--- script/photos.js
+++ script/photos.js
@@ -3,20 +3,20 @@ document.querySelectorAll('.circle-card').forEach(card => {
       card.classList.toggle('flipped');
     });
   });
-
+  
   let currentIndex = 0;
-
+  
   function changeSlide(direction) {
     const slide = document.querySelector('.carousel-slide');
     const totalSlides = document.querySelectorAll('.carousel-img').length;
   
     currentIndex += direction;
   
-    // Si llegas al final o al principio, regresa en ciclo
+    // Carrusel cíclico
     if (currentIndex < 0) currentIndex = totalSlides - 1;
     if (currentIndex >= totalSlides) currentIndex = 0;
   
-    const offset = -currentIndex * 300; // 300 es el ancho de cada imagen
+    const offset = -currentIndex * 320; // 300px + 2*10px margin
     slide.style.transform = `translateX(${offset}px)`;
   }
   
